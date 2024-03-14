b29a5537e359b13aa0908deeb5cd51cc9422f4d2
Koby Fink <finkkoby@icloud.com>
2024-03-14 13:20:23 -0400

    complete lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
 # your code goes here!
+import time
+
+def countdown(int):
+    i = int
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+    print("HAPPY NEW YEAR!")
+    return None
+
+def countdown_with_sleep(int):
+    i = int
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+    return None
